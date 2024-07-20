bf6189f65f3d5b955e4bef674ac96e63c0b11d35
Palak_Yadav <sy8220718@gmail.com>
2024-07-20 23:40:15 +0530

    Commit ex_20072024

---
+++ src/ex_20072024/Lab083.java
@@ -0,0 +1,17 @@
+package ex_20072024;
+
+public class Lab083 {
+    public static void main(String[] args) {
+        // a = 4 -> TRUE OR FALSE
+        int a = 10;
+
+        if(a==4)
+        {
+            System.out.println("FALSE");
+        }
+        else{
+            System.out.println("TRUE");
+        }
+    }
+
+}
